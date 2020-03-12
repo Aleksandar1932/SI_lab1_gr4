3a41513605331cf0181b65101572eb2d15218d96
Stefan Andonov <stefan5andonov@yahoo.com>
2020-03-12 18:00:07 +0100

    initial commit

---
+++ Point.java
@@ -0,0 +1,21 @@
+class Point {
+	String id
+	double x,y;
+
+	//TODO add new variable
+
+	//TODO constructor
+
+	//TODO setters and getters
+
+	public void move (char xDirection, char yDirection) {
+		//TODO
+	}
+
+	public void draw () {
+		//TODO
+	}
+
+
+
+}
