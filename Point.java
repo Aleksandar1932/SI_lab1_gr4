5e7e60fc808b71ab44e238502b0495b0b780e202
aleksandar <aleksandar.ivanovski123@gmail.com>
2020-03-23 19:52:20 +0100

    point class finished

--- Point.java
+++ Point.java
@@ -3,18 +3,68 @@ class Point {
 	String color;
 	double x,y;
 
-	//TODO constructor
+	public Point(String id, String color, double x, double y){
+		this.id = id;
+		this.color = color;
+		this.x = x;
+		this.y = y;
+	}
 
-	//TODO setters and getters
+	//Setters:
+	
+	public void setId(String newID){
+		this.id = newID;	
+	}
+
+	public void setColor(String newColor){
+		this.color = newColor;
+	}
+
+	public void setX(double newX){
+		this.x = newX;
+	}
+
+	public void setY(double newY){
+		this.y = newY;
+	}
+
+	//Getters:
+
+	public String getId(){
+		return this.id;
+	}
+
+	public String getColor(){
+		return this.color;
+	}
+
+	public double getX(){
+		return this.x;
+	}
+
+	public double getY(){
+		return this.y;
+	}
 
 	public void move (char xDirection, char yDirection) {
-		//TODO
+		if(xDirection == 'L'){
+			this.x -= 1;
+		}
+		else if(xDirection == 'R'){
+			this.x += 1;
+		}
+
+		if(yDirection == 'U'){
+			this.y += 1;
+		}
+		else if(yDirection == 'D'){
+			this.y -= 1;
+		}
+
 	}
 
 	public void draw () {
-		//TODO
+		System.out.println(String.format("(x = %d, y = %d)",x,y));
 	}
-
-
 
 }
